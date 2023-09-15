2e8304e1dd9f36df486fc211b1f353aa6c78ce37
Lena <lena@Lenas-MacBook-Pro.local>
2023-09-14 17:25:47 -0700

    removed uncaught exception handling

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,7 +1,1 @@
 import "./commands";
-
-// If rerunning tests on https://www.cypress.io/ multiple times, some XHR requests start to fail causing the test to fail.
-Cypress.on("uncaught:exception", () => {
-  // returning false here prevents Cypress from failing the test
-  return false;
-});
