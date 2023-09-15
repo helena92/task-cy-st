158a28dc4f65a88984f1927e7243cc9d1858f99d
Lena <lena@Lenas-MacBook-Pro.local>
2023-09-14 17:10:09 -0700

    added ci job with reporting

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  // https://github.com/adamgruber/mochawesome
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    reportFilename: '[name].html',
+    overwrite: true,
+    html: true,
+    json: false,
+  },
   e2e: {
     baseUrl: 'http://stackadapt-interview.us-east-1.elasticbeanstalk.com',
   },
